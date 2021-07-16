436e35941ccc8ea1b12c6c113fcd95be2526b89c
yprich <74568602+yprich@users.noreply.github.com>
2021-07-16 14:39:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ end_date = st.sidebar.date_input("End date", datetime.date(2021, 1, 31))
 ticker_list = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/s-and-p-500-companies/master/data/constituents_symbols.txt')
 tickerSymbol = st.sidebar.selectbox('Stock ticker', ticker_list) # Select ticker symbol
 tickerData = yf.Ticker(tickerSymbol) # Get ticker data
-tickerDf = tickerData.history(period='1d', start=start_date, end=end_date) #get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start=start_date, end=end_date)
 
-# Ticker information
+
 string_logo = '<img src=%s>' % tickerData.info['logo_url']
 st.markdown(string_logo, unsafe_allow_html=True)
 
